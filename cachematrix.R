88ade868dfbb867f72b8c0eb21a0674fa96b8752
Fer <fperezgarciagm@gmail.com>
2014-04-15 13:12:32 +0200

    Initial Commit
    Initial Commit for PA2

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,6 @@
-## Put comments here that give an overall description of what your
-## functions do
+## ProgrammingAssignment2
+## Author: Fernando Perez
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
 
