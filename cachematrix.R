9907e0e5df8284e5c6103523073c4965ab7508e6
Fer <fperezgarciagm@gmail.com>
2014-04-21 17:45:45 +0200

    Added comments
    Added comments in the functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,15 @@
 ## ProgrammingAssignment2
 ## Author: Fernando Perez
+## Test this functions with: test_cachematrix.R
 
+## makeCacheMatrix returns the functions to handle the matrix inversion
+## set -> stores the original Matrix
+## get -> recovers the original Matrix
+## setInverse -> stores the Inverse Matrix
+## getInverse -> recovers the Inverse Matrix
 
 makeCacheMatrix <- function(mMat = matrix()) {
-        ## 
-        ## 
-        ##
+
         
         mInv <- NULL
         
@@ -26,25 +30,28 @@ makeCacheMatrix <- function(mMat = matrix()) {
                 mInv
         }
         
-        ## Returns a list of 4 funtions to handle the mean
+        ## Returns the list of 4 funtions to handle the matrix inversion
         list(set = set, get = get,
              setInverse = setInverse,
              getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve returns the inverse matrix of the input parameter matrix (mMat)
+## if the inverse has already been solved, it is recovered from cache
 
 cacheSolve <- function(mMat, ...) {
         
-        ## Return a matrix that is the inverse of 'x'
+        ## 1. Get Inverse from cache
         mInv <- mMat$getInverse()
         
+        # 2. If Inverse is not null (already computed), return Inverse
         if(!is.null(mInv)) {
                 message("getting cached inverse matrix")
                 return(mInv)
         }
         
+        # 3. Get Matrix from Cache and compute Inverse, if Matrix is square
         dataMatrix <- mMat$get()
         if(dim(dataMatrix)[1] != dim(dataMatrix)[2]) {
                 message("the argument must be a square matrix")
@@ -52,8 +59,11 @@ cacheSolve <- function(mMat, ...) {
         }
         
         mInv <- solve(dataMatrix, ...)
+        
+        # 4. Store Inverse in cache
         mMat$setInverse(mInv)
         
+        # 5. Return Inverse
         mInv
 }
 
